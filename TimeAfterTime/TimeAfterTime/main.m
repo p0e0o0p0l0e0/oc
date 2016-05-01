a65dd918363c7b815fa21f6bf45abab5d9dd2bfe
Linqi Zhang <lqzhang@kabaminc.com>
2016-05-01 19:08:34 +0800

    Chapter 14.3 Nesting message sends

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -14,8 +14,11 @@ int main(int argc, const char * argv[]) {
 		NSDate *now = [NSDate date];
 		NSLog(@"This NSDate object lives at %p", now);
 		NSLog(@"This date is %@", now);
-		double seconds = [now timeIntervalSince1970];
+		
+//		double seconds = [now timeIntervalSince1970];
+		double seconds = [[NSDate date] timeIntervalSince1970];
 		NSLog(@"It has been %f seconds since the start of 1970.", seconds);
+		
 		
 		NSDate *later = [now dateByAddingTimeInterval:100000];
 		NSLog(@"In 100,000 seconds it will be %@", later);
