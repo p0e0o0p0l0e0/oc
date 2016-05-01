f633846d44299d35d5b9d1ab397dfc895a1a3a7b
Linqi Zhang <lqzhang@kabaminc.com>
2016-05-01 20:14:43 +0800

    Chapter 15.1 On Pointers and their valus

---
+++ TimesTwo/TimesTwo/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by linqi zhang on 16/5/1.
+//  Copyright © 2016年 Linqi Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+		
+		NSDate *currentTime = [NSDate date];
+		NSLog(@"currentTime's value is %p", currentTime);
+		
+		NSDate *startTime = currentTime;
+		
+		sleep(2);
+		
+		currentTime = [NSDate date];
+		NSLog(@"currentTime's value is now %p", currentTime);
+		NSLog(@"The address of the orignal object is %p", startTime);
+	}
+    return 0;
+}
