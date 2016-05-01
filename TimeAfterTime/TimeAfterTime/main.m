0afe71ad142899a32626415dee328504c39e8145
Linqi Zhang <lqzhang@kabaminc.com>
2016-05-01 19:35:40 +0800

    Chapter 14.4 alloc and init

--- TimeAfterTime/TimeAfterTime/main.m
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -11,12 +11,15 @@
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
 	    // insert code here...
-		NSDate *now = [NSDate date];
+//		NSDate *now = [NSDate date];
+		NSDate *now = [[NSDate alloc]init];
+		
 		NSLog(@"This NSDate object lives at %p", now);
 		NSLog(@"This date is %@", now);
 		
 //		double seconds = [now timeIntervalSince1970];
 		double seconds = [[NSDate date] timeIntervalSince1970];
+		
 		NSLog(@"It has been %f seconds since the start of 1970.", seconds);
 		
 		
