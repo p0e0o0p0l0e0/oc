e5e571ba7960bb9d86170d1ee129c9c86b556372
Linqi Zhang <lqzhang@kabaminc.com>
2016-06-03 13:28:39 +0800

    Chapter 27.1 The run loop.

---
+++ Callbacks/Callbacks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Callbacks
+//
+//  Created by linqi zhang on 16/6/3.
+//  Copyright © 2016年 Linqi Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+		
+		[[NSRunLoop currentRunLoop] run];
+	}
+    return 0;
+}
